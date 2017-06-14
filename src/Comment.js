ed828d790db91ff6f06c42ed547ef48ca6cbbdd2
hejin <1070731001@qq.com>
2017-06-14 23:53:51 +0800

    add comments and store username and content in localstorage

---
+++ src/Comment.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+class Comment extends Component {
+  static propTypes = {
+    comment: PropTypes.object
+  }
+  constructor () {
+    super()
+    this.state = {
+      timeString: ''
+    }
+  }
+  componentWillMount () {
+    this._updateTimeString()
+    this._timer = setInterval(
+      this._updateTimeString.bind(this), 5000
+    )
+  }
+  _updateTimeString () {
+    const comment = this.props.comment
+    const duration = (+Date.now()-comment.createdTime)/1000
+    this.setState({
+      timeString: duration > 60?
+      `${Math.round(duration / 60)}分钟前`
+      :`${Math.round(duration)}秒前`
+    })
+  }
+  render () {
+    return (
+      <div className='comment'>
+        <div className='comment-user'>
+          <span className='comment-username'>{this.props.comment.username} </span>：
+        </div>
+        <p>{this.props.comment.content}</p>
+        <span className='comment-createdtime'>
+          {this.state.timeString}
+        </span>
+        <span className='comment-delete'>
+          删除
+        </span>
+      </div>
+    )
+  }
+}
+
+export default Comment
